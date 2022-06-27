58c3b8df7ce24edbc8b2964cbf69c385f2a738da
Alena Komarova <Alena.Komarova@jetbrains.com>
2022-06-27 11:58:41 +0300

    add an itemgroup

--- SimpleTests/Class1.cs
+++ SimpleTests/Class1.cs
@@ -13,28 +13,26 @@ namespace SimpleTests
 		//SA1508 - closing braces should not be preceded by a blank line
 		//SA1002 - space before semicolon
 		//SA1008 - opening parenthesis should not be followed by a space
-		public object Method( )
+		public object Method()
 		{
 			lock (this)
 			{
 				return this.value;
-
 			}
 		}
+
 		//SA1111 closing parenthesis should be on line of last parameter
-	
+
 		public string JoinName(string first, string last)
 		{
-			string name = JoinStrings(
-			                          first,
-			                          last
-			                          );
+			string name = JoinStrings(first,
+				last);
 			return name;
 		}
 
 		private string JoinStrings(string first, string last)
 		{
-			return(first + " " + last);
+			return (first + " " + last);
 		}
 	}
 }
